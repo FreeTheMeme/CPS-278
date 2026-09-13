efcce672fae06d26507b743bf98463791dac7125
bigtin <bigtin@protonmail.com>
2026-09-13 12:56:58 -0400

    Pizza done for now...

--- mp1_aurewhite/src/main/java/edu/wccnet/aurewhite/Pizza.java
+++ mp1_aurewhite/src/main/java/edu/wccnet/aurewhite/Pizza.java
@@ -6,10 +6,33 @@ public class Pizza {
     char size;
 
     // Number of toppings
+    private String[] toppings;
     // and finally toppings I'm not sure how we're gonna store all the toppings,
     // one long string, maybe an array of string strings
 
-    // getters and setters
     // constructor
 
+    public Pizza(char size, String[] toppings) {
+        this.size = size;
+        this.toppings = toppings;
+    }
+
+    // getters and setters
+
+    // size
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+    // toppings
+    public String[] getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(String[] toppings) {
+        this.toppings = toppings;
+    }
 }
